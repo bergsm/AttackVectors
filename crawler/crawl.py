625383db58678be4c3f5c26cd1b7b6733be0666b
Shawn Berg <bergshawnm@gmail.com>
2020-02-21 13:30:35 -0500

    Stubbed crawler script

---
+++ crawler/crawl.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+'''
+Adapted from psuedocode in CLRS chapter 22.2, third edition. This function performs a breadth-first search on the graph. Source for Beautiful Soup: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+'''
+import sys
+import os
+import urllib
+from bs4 import BeautifulSoup
+from urlparse import urljoin
+from itertools import cycle
+import Cookie
+
+
+# Take in starting url as argument
+
+# Parse urls for other publications (footer__list row)\
+
+# Perform BFS on urls until no new urls are found
+
+# Publish list
