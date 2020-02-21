11987469bda3acbe13f2f30c7cb455e74c7f4e4f
Shawn Berg <bergshawnm@gmail.com>
2020-02-21 14:26:33 -0500

    Parsing links, bfs stubbed

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -4,18 +4,57 @@
 Adapted from psuedocode in CLRS chapter 22.2, third edition. This function performs a breadth-first search on the graph. Source for Beautiful Soup: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 '''
 import sys
+import requests
 import os
 import urllib
-from bs4 import BeautifulSoup
-from urlparse import urljoin
-from itertools import cycle
-import Cookie
+from bs4 import BeautifulSoup as BS
+from urllib.parse import urljoin
+
+# needed to access sites. 403 error without user agent
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0'}
 
 
-# Take in starting url as argument
+#TODO Take in starting url as argument
 
-# Parse urls for other publications (footer__list row)\
+#def parseLinks(url, links):
+def parseLinks(url):
+    try:
+        r = requests.get(url, headers=headers)
+        page = r.content
+        soup = BS(page, 'html.parser')
+        #footer = soup.find('ul',{'class':'footer__list row'}).findAll('a')
+        links = []
+        for link in soup.find('ul',{'class':'footer__list row'}).findAll('a'):
+            links.append(urljoin(url, link.get('string')))
+            links.append(link.string)
+        print (links)
+        #print (soup)
+    except Exception as e:
+        sys.stderr.write("Error: " + str(e))
+
 
 # Perform BFS on urls until no new urls are found
 
-# Publish list
+def bfs(links, start):
+    visited = []
+    queue = [start]
+
+    while queue:
+
+        node = queue.pop(0)
+        if node not in visited:
+            visited.append(node)
+            neighbors = links[node]
+
+            for neighbor in neighbor:
+                queue.append(neighbor)
+        return visited
+
+# Publish list to README?
+
+# Future TODO present graphical representation of sites found
+
+
+start = 'https://northmichigannews.com'
+
+parseLinks(start)
